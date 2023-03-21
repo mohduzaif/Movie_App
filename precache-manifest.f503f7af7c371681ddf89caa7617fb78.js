23dd7a32540b37529b807265dd5257832f4673f1
mohduzaif <mohduzaif192@gmail.com>
2023-03-21 16:06:02 +0530

    Updates

---
+++ precache-manifest.f503f7af7c371681ddf89caa7617fb78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbefb861a5ccaf74fa79",
+    "url": "mohduzaif.github.io/movie/static/css/main.cfb3883e.chunk.css"
+  },
+  {
+    "revision": "dbefb861a5ccaf74fa79",
+    "url": "mohduzaif.github.io/movie/static/js/main.dbefb861.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "mohduzaif.github.io/movie/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "990b784b94b580f635f7",
+    "url": "mohduzaif.github.io/movie/static/js/2.990b784b.chunk.js"
+  },
+  {
+    "revision": "0dd713e0171d5f4ef2e3",
+    "url": "mohduzaif.github.io/movie/static/js/runtime~main.0dd713e0.js"
+  },
+  {
+    "revision": "3abfcbb8bf74cb39624596093e3dc442",
+    "url": "mohduzaif.github.io/movie/index.html"
+  }
+];
